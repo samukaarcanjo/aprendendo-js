cac3a42bb0daf7ae7cda8bc2c7b34a25a03d5760
SamukaArcanjo <samuelarcanjo57@gmail.com>
2026-05-25 20:50:48 -0300

    condições if e else

--- script.js
+++ script.js
@@ -2,13 +2,19 @@
 // let idade = 24
 // let falsetrue = true
 
-let nome = "joão"
-let sobrenome = " Bonete"
-let nometotal = nome+sobrenome
+let nome = "joão";
+let sobrenome = " Bonete";
+let nometotal = nome+sobrenome;
 
-document.write(nometotal)
-let num1 = 23
-let num2 = 12
-let resultado = num1 + num2
+document.write(nometotal);
 
-document.write(resultado)
+
+let num1 = 23;
+let num2 = 12;
+let resultado = num1 + num2;
+
+if (num1 >= num2) {
+    console.log("Verdade")
+}else {
+    console.log("Falso")
+}
