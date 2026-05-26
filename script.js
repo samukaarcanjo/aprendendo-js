d7b304b715e6ea007e5c2d0dfe759729cdb1a745
SamukaArcanjo <samuelarcanjo57@gmail.com>
2026-05-26 18:44:46 -0300

    tipos de variaveis e funções

--- script.js
+++ script.js
@@ -1,20 +1,9 @@
-// let nome = "Guilherme"
-// let idade = 24
-// let falsetrue = true
-
-let nome = "joão";
-let sobrenome = " Bonete";
-let nometotal = nome+sobrenome;
-
-document.write(nometotal);
+// tipo de variaveis
+// var , let , const
 
 
-let num1 = 23;
-let num2 = 12;
-let resultado = num1 + num2;
-
-if (num1 >= num2) {
-    console.log("Verdade")
-}else {
-    console.log("Falso")
+function nomeDafuncao() {
+    console.log("Hello World")
 }
+
+nomeDafuncao()
