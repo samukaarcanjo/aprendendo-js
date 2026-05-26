5c7cc69ff0308d89380a45be4fb1e9ac2daac31f
SamukaArcanjo <samuelarcanjo57@gmail.com>
2026-05-26 20:27:31 -0300

    exercicio de encontrar o maior numero. Facil facil facil

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 // Contador letras especificas
 
-let numeros = [10,20,30,40]
-let soma = 0
+let numeros = [101,20,30,40]
+let maior_numero = numeros[0]
 for (let i = 0 ; i <= numeros.length - 1; i ++){
-    soma += numeros[i]
+    if (numeros[i] > maior_numero ){
+        maior_numero = numeros[i]
+    }
 }
-console.log(soma)
+console.log(maior_numero)
