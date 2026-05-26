10f00ed8f87dc8bfb2110fcdc2bc16d9c54c7e5b
SamukaArcanjo <samuelarcanjo57@gmail.com>
2026-05-26 18:50:15 -0300

    Exercicio de JS feito

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 // tipo de variaveis
 // var , let , const
 
+// function nomeDafuncao() {
+//     console.log("Hello World")
+// }
 
-function nomeDafuncao() {
-    console.log("Hello World")
+// nomeDafuncao()
+// Exercicio com for 
+let nome = "Samuel";
+let nome_contrario = "";
+for (let i = nome.length - 1 ; i >= 0 ; i--){
+    nome_contrario += nome[i];
 }
-
-nomeDafuncao()
+console.log(nome_contrario)
