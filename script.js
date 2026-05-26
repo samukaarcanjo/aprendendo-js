b9658605a62f4757b5bcf7cb9a11c2126eee27e9
SamukaArcanjo <samuelarcanjo57@gmail.com>
2026-05-26 20:31:33 -0300

    Descobri menor numero da array... Facil facil facil

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-// Contador letras especificas
+// Descobrir menor numero da array
 
-let numeros = [101,20,30,40]
-let maior_numero = numeros[0]
+let numeros = [101,20,30,40,-5]
+let menor_numero = numeros[0]
+
 for (let i = 0 ; i <= numeros.length - 1; i ++){
-    if (numeros[i] > maior_numero ){
-        maior_numero = numeros[i]
+    if (numeros[i] < menor_numero ){
+        menor_numero = numeros[i]
     }
 }
-console.log(maior_numero)
+console.log(menor_numero)
