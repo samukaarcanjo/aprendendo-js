03a0e4cb647d2313e79ee3a474e0765e0e84ac3a
SamukaArcanjo <samuelarcanjo57@gmail.com>
2026-05-26 22:08:59 -0300

    ESSE EU QUEBREI A CABEÇA LEGAL.... Eram dois loops, varios ifs... Mas, muito legal! Achei (Dificil)

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
-// Filtrar dados. Criar uma array que contenha alguns numeros e filtras apenas os pares.
+// Filtrar dados. Analizar as duplicatas e entregar uma array que tenha apenas as duplicatas. Mas, apenas uma delas, não as duas iguais 
 
-let numeros = [10,11,10,20,11,20,500,34]
+let numeros = [10,34,10,20,43,11,20,500,34]
+let duplicatas = []
 
+for (let i = 0; i <= numeros.length -1; i ++){ 
+    for (let j = 0; j <= numeros.length -1; j ++){
+        if (numeros[i] == numeros[j] && i != j) {
+            if (duplicatas.includes(numeros[i]) == false) {
+                duplicatas.push(numeros[i])
+            }
+        }
+    }
+}
+
+console.log(duplicatas)
