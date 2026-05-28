4038acf729f41030267f22c11199f8dfe3cf11c1
SamukaArcanjo <samuelarcanjo57@gmail.com>
2026-05-28 16:21:11 -0300

    ultimo exercicio agora corrigido. Não tinha declarado o valor em somaImpares

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
-// Usando reduce()
+// Usando map,filter,reduce para filtrar pares e impares, dobrar valores e somar todos 
 
-let numeros = [5,10,15]
+let numeros = [1,2,3,4,5,6]
 
-let somaTodos = numeros.reduce((somatodos,numero) => {
+let pares = numeros.filter((numero) => {
+    return numero % 2 === 0
+})
+let impares = numeros.filter((numero) => {
+    return numero % 2 != 0
+})
+
+let dobroPares = pares.map((numero) => {
+    return numero * 2
+})
+
+let dobroImpares = impares.map((numero) => {
+    return numero * 2
+})
+
+let somaPares = dobroPares.reduce((somatodos,numero) => {
+    return somatodos + numero
+},0)
+let somaImpares = dobroImpares.reduce((somatodos,numero) => {
     return somatodos + numero
 },0)
 
-console.log(somaTodos)
+console.log(somaPares)
+console.log(somaImpares)
