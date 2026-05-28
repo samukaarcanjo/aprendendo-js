3101862759508cbfe30acd24e38287458b4b6146
SamukaArcanjo <samuelarcanjo57@gmail.com>
2026-05-28 11:00:42 -0300

    Criar função de soma

--- script.js
+++ script.js
@@ -1,14 +1,8 @@
-// Contar frequencia. Contar a frequencia que cada numero aparece dentro da lista. Adicionalo a um objeto e somar sua frequencia. 
+// criar função de soma
 
-let numeros = [10,34,10,20,43,11,20,500,34]
-let frequencia = {}
-
-for (let i = 0; i <= numeros.length -1; i ++) {
-    if (frequencia[numeros[i]]) {
-        frequencia[numeros[i]] += 1
-    }
-    else {
-        frequencia[numeros[i]] = 1
-    }
+function soma(a,b) {
+    return a + b
 }
-console.log(frequencia)
+
+let resultado = soma(40,20)
+console.log(resultado)
