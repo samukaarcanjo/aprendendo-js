7b7fc16881f209315a879d29dd20c478b2fec8a3
SamukaArcanjo <samuelarcanjo57@gmail.com>
2026-05-28 15:47:58 -0300

    Usando Filter()

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-// Usando Map()
+// Usando Filter()
 
 let numeros = [1,2,3,4,5]
 
-let aoQuadrado = numeros.map((numero) => {
-    return numero * numero
+let pares = numeros.filter((numero) => {
+    return numero % 2 != 0
 })
 
-console.log(aoQuadrado)
+console.log(pares)
