34aeb843bb7632b1fbde0e76ee162de0809403d2
SamukaArcanjo <samuelarcanjo57@gmail.com>
2026-05-29 09:27:21 -0300

    Exercicio reduce

--- script.js
+++ script.js
@@ -1,23 +1,16 @@
-const produtos = [
-    { nome: "Notebook", preco: 3500, categoria: "tecnologia" },
-    { nome: "Mouse", preco: 120, categoria: "tecnologia" },
-    { nome: "Cadeira", preco: 900, categoria: "moveis" },
-    { nome: "Teclado", preco: 250, categoria: "tecnologia" },
-    { nome: "Mesa", preco: 700, categoria: "moveis" }
+const pedidos = [
+    { produto: "Notebook", preco: 3500 },
+    { produto: "Mouse", preco: 120 },
+    { produto: "Teclado", preco: 250 }
 ]
 
 // Objetivo:
-// Crie uma variável que contenha apenas os produtos
-// da categoria "tecnologia"
-let tecnologia = produtos.filter((item) => {
-    return item.categoria === "tecnologia"
-})
-
-console.log(tecnologia)
+// Somar o valor total dos produtos.
 //
+let precoTot = pedidos.reduce((somatodos,produto) => {
+    return somatodos += produto.preco
+},0)
+
+console.log(precoTot)
 // Resultado esperado:
-// [
-//   { nome: "Notebook", preco: 3500, categoria: "tecnologia" },
-//   { nome: "Mouse", preco: 120, categoria: "tecnologia" },
-//   { nome: "Teclado", preco: 250, categoria: "tecnologia" }
-// ]
+// 3870
