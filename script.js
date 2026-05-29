4f26765aa91d8b9a5dbc78d95934ee9e8f7a565b
SamukaArcanjo <samuelarcanjo57@gmail.com>
2026-05-29 09:22:22 -0300

    exercicio filter

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
-// find()
-
-let usuarios = [
-    { nome: "Samuel", idade: 22 },
-    { nome: "Lucas", idade: 17 },
-    { nome: "Ana", idade: 25 }
+const produtos = [
+    { nome: "Notebook", preco: 3500, categoria: "tecnologia" },
+    { nome: "Mouse", preco: 120, categoria: "tecnologia" },
+    { nome: "Cadeira", preco: 900, categoria: "moveis" },
+    { nome: "Teclado", preco: 250, categoria: "tecnologia" },
+    { nome: "Mesa", preco: 700, categoria: "moveis" }
 ]
 
-let nome = usuarios.find(usuario => usuario.nome === "Ana")
-console.log(nome.idade)
+// Objetivo:
+// Crie uma variável que contenha apenas os produtos
+// da categoria "tecnologia"
+let tecnologia = produtos.filter((item) => {
+    return item.categoria === "tecnologia"
+})
+
+console.log(tecnologia)
+//
+// Resultado esperado:
+// [
+//   { nome: "Notebook", preco: 3500, categoria: "tecnologia" },
+//   { nome: "Mouse", preco: 120, categoria: "tecnologia" },
+//   { nome: "Teclado", preco: 250, categoria: "tecnologia" }
+// ]
