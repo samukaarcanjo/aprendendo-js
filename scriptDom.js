514b945b285bff5e50e2557a73a28c2b627c7b20
SamukaArcanjo <samuelarcanjo57@gmail.com>
2026-05-29 21:13:05 -0300

    Lista de Tarefas (Bem Simples)
    estou feliz em sentir que estou avançando muito

--- scriptDom.js
+++ scriptDom.js
@@ -1,9 +1,35 @@
-let nome = document.getElementById("nome")
+let adicionar_tarefas = document.getElementById("adicionar_tarefas")
 
 let botao = document.getElementById("botao")
-let titulo = document.getElementById("titulo")
+
+let lista = document.getElementById("lista")
 
 botao.addEventListener("click", () => {
-    console.log(nome.value)
-    titulo.innerText = `Olá ${nome.value}`
+
+    if (adicionar_tarefas.value.trim() === "") {
+        alert("Por favor digite uma taréfa ")
+        return
+    } 
+
+    let item = document.createElement("li")
+    item.innerText = adicionar_tarefas.value
+
+    let botao_excluir = document.createElement("button")
+    botao_excluir.innerText = "Excluir"
+
+    botao_excluir.addEventListener("click", () => {
+        item.remove()
+    })
+
+    item.appendChild(botao_excluir)
+    lista.appendChild(item)
+
+    adicionar_tarefas.value = ""
 })
+
+adicionar_tarefas.addEventListener("keydown", (evento) => {
+    if (evento.key === "Enter") {
+        botao.click()
+    }
+})
+
